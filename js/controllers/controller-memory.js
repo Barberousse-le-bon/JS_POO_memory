63eaa3ee7f8539410757f8359451e7c9f0ace117
HYOT Lucas <lucash21200303@gmail.com>
2024-05-14 16:14:34 +0200

    création du controller-memory avec les cartes

--- js/controllers/controller-memory.js
+++ js/controllers/controller-memory.js
@@ -1,11 +1,33 @@
 import { Card } from "../models/card.js";
-import { Memory } from "../models/memory.js";
+//import { Memory } from "../models/memory.js";
 import { Notifier } from "../patterns/notifier.js";
 
 export class ControllerMemory extends Notifier
 {
+
+    #card;
     constructor()
     {
         super();
+    }
+
+
+    getCard(){
+
+
+        return this.#card.getCard();
+
+    }
+
+
+    createCard(){
+
+
+        value = Math.random()*0xF3+0x1f90c;
+
+        console.log(value);
+
+        this.#card = new Card(value);
+
     }
 }
